f01fce70c8d163e5c99086a598acf489b8186008
JONGJIN-LEE1 <jin1122877@naver.com>
2025-09-22 13:29:20 +0900

    fix: API 명세서 수정본대로 수정

--- src/votes/dto/vote-response.dto.ts
+++ src/votes/dto/vote-response.dto.ts
@@ -1,18 +1,8 @@
+import { OptionResult } from "../../polls/dto/option-response.dto";
+
 export interface VoteResponseDto {
   message: string;
-  updatedOption: {
-    id: string;
-    title: string;
-    votes: number;
-  };
-  winnerOption?: {
-    id: string;
-    title: string;
-    votes: number;
-  };
-  options: Array<{
-    id: string;
-    title: string;
-    votes: number;
-  }>;
+  updatedOption?: OptionResult;
+  winnerOption?: OptionResult;
+  options?: OptionResult[];
 }
