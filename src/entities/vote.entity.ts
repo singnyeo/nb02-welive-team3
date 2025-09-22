5b098866e3c654b96a2b30df806536170c867854
JONGJIN-LEE1 <jin1122877@naver.com>
2025-09-22 13:29:20 +0900

    fix: API 명세서 수정본대로 수정

--- src/entities/vote.entity.ts
+++ src/entities/vote.entity.ts
@@ -4,42 +4,37 @@ import {
   Column,
   ManyToOne,
   CreateDateColumn,
-  Unique,
   JoinColumn,
+  Index,
 } from "typeorm";
-import { User } from "./user.entity";
-import { Poll } from "./poll.entity";
 import { PollOption } from "./poll-option.entity";
+import { User } from "./user.entity";
 
 @Entity("votes")
-@Unique(["userId", "pollId"])
+@Index(["userId", "pollId"], { unique: true }) // 한 투표당 사용자 1회만 투표 가능
 export class Vote {
   @PrimaryGeneratedColumn("uuid")
   id!: string;
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: "userId" }) // 여기서 userId는 입주민 ID
+  @ManyToOne(() => User, (user) => user.votes)
+  @JoinColumn({ name: "userId" })
   user!: User;
 
-  @Column({ type: "uuid" })
+  @Column()
   userId!: string;
 
-  @ManyToOne(() => Poll)
-  @JoinColumn({ name: "pollId" })
-  poll!: Poll;
-
-  @Column({ type: "uuid" })
-  pollId!: string;
-
-  @ManyToOne(() => PollOption, (option: PollOption) => option.voteRecords, {
+  @ManyToOne(() => PollOption, (option) => option.votes, {
     onDelete: "CASCADE",
   })
   @JoinColumn({ name: "optionId" })
   option!: PollOption;
 
-  @Column({ type: "uuid" })
+  @Column()
   optionId!: string;
 
+  @Column()
+  pollId!: string; // 중복 투표 방지를 위한 pollId 추가
+
   @CreateDateColumn()
-  votedAt!: Date;
+  createdAt!: Date;
 }
