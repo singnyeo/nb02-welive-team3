50f91e9be9f1299762903b39d71a0d1133bef236
gyunam-bark <gyunam.bark@gmail.com>
2025-09-22 13:48:22 +0900

    feat: signup,login,logout,refresh API 초안 구현

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,23 +1,19 @@
 import express from 'express';
 import { allow, AllowedRole } from '../middlewares/allow.middleware';
 import {
-  handleApproveAdmin,
-  handleApproveAdmins,
-  handleApproveUser,
-  handleApproveUsers,
   handleCleanup,
   handleDeleteAdmin,
   handleLogin,
   handleLogout,
   handleRefresh,
-  handleRejectAdmin,
-  handleRejectAdmins,
-  handleRejectUser,
-  handleRejectUsers,
   handleSignup,
   handleSignupAdmin,
   handleSignupSuperAdmin,
   handleUpdateAdmin,
+  handleUpdateAdminsStatus,
+  handleUpdateAdminStatus,
+  handleUpdateResidentsStatus,
+  handleUpdateResidentStatus,
 } from './auth.controller';
 
 const auth = express.Router();
@@ -28,16 +24,12 @@ auth.post('/signup/super-admin', allow(AllowedRole.NONE), handleSignupSuperAdmin
 auth.post('/login', allow(AllowedRole.NONE), handleLogin);
 auth.post('/logout', allow(AllowedRole.USER), handleLogout);
 auth.post('/refresh', allow(AllowedRole.USER), handleRefresh);
-auth.post('/approve-admin', allow(AllowedRole.SUPER_ADMIN), handleApproveAdmin);
-auth.post('/reject-admin', allow(AllowedRole.SUPER_ADMIN), handleRejectAdmin);
-auth.post('/approve-admins', allow(AllowedRole.SUPER_ADMIN), handleApproveAdmins);
-auth.post('/reject-admins', allow(AllowedRole.SUPER_ADMIN), handleRejectAdmins);
-auth.post('/approve-user/:residentId', allow(AllowedRole.ADMIN), handleApproveUser);
-auth.post('/reject-user/:residentId', allow(AllowedRole.ADMIN), handleRejectUser);
-auth.post('/approve-users', allow(AllowedRole.ADMIN), handleApproveUsers);
-auth.post('/reject-users', allow(AllowedRole.ADMIN), handleRejectUsers);
-auth.patch('/update-admin', allow(AllowedRole.SUPER_ADMIN), handleUpdateAdmin); // [!] id 가 body 에 들어가서 처리됨.
-auth.patch('/delete-admin/:adminId', allow(AllowedRole.SUPER_ADMIN), handleDeleteAdmin);
-auth.post('/cleanup', allow(AllowedRole.SUPER_ADMIN), handleCleanup);
+auth.patch('/admins/:id/status', allow(AllowedRole.SUPER_ADMIN), handleUpdateAdminStatus);
+auth.patch('/admins/status', allow(AllowedRole.SUPER_ADMIN), handleUpdateAdminsStatus);
+auth.patch('/residents/:id/status', allow(AllowedRole.ADMIN), handleUpdateResidentStatus);
+auth.patch('/residents/status', allow(AllowedRole.ADMIN), handleUpdateResidentsStatus);
+auth.patch('/admins/:id', allow(AllowedRole.SUPER_ADMIN), handleUpdateAdmin);
+auth.delete('/admins/:id', allow(AllowedRole.SUPER_ADMIN), handleDeleteAdmin);
+auth.post('/cleanup', allow(AllowedRole.ADMIN), handleCleanup);
 
 export default auth;
