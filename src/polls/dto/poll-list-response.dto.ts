0009b7a4e926217b13d61d2c57775630948e0118
JONGJIN-LEE1 <jin1122877@naver.com>
2025-09-22 14:09:33 +0900

    feat: poll 관련 DTO 작성

---
+++ src/polls/dto/poll-list-response.dto.ts
@@ -0,0 +1,17 @@
+export interface PollListResponseDto {
+  pollId: string;
+  userId: string;
+  title: string;
+  writerName: string;
+  buildingPermission?: string;
+  createdAt: string;
+  updatedAt: string;
+  startDate: string;
+  endDate: string;
+  status: "IN_PROGRESS" | "PENDING" | "COMPLETED";
+}
+
+export interface PollsListWrapperDto {
+  polls: PollListResponseDto[];
+  totalCount: number;
+}
