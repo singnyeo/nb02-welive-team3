e221804a03192062f26d09821067336cbc333719
siyeon kang <rkdtldus0115@naver.com>
2025-09-23 09:13:59 +0900

    feat: 엔티티 초안 작성

---
+++ src/entities/comment.entity.ts
@@ -0,0 +1,60 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Index,
+} from "typeorm";
+import { User } from "./user.entity";
+import { Complaint } from "./complaint.entity";
+// import { Notice } from "./notice.entity";
+
+export enum BoardType {
+  COMPLAINT = "COMPLAINT",
+  NOTICE = "NOTICE",
+}
+
+@Entity({ name: "comments" })
+@Index(["boardType", "boardId"])
+export class Comment {
+  @PrimaryGeneratedColumn("uuid")
+  commentId!: string;
+
+  @Column({ type: "text" })
+  content!: string;
+
+  @ManyToOne(() => User, (user) => user.comments, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  @Column({ type: "uuid" })
+  userId!: string;
+
+  @Column()
+  writerName!: string;
+
+  @Column({ type: "enum", enum: BoardType })
+  boardType!: BoardType;
+
+  @Column({ type: "uuid" })
+  boardId!: string; // complaintId | noticeId
+
+  // 민원
+  @ManyToOne(() => Complaint, (complaint) => complaint.comments, { nullable: true, onDelete: "CASCADE" })
+  @JoinColumn({ name: "complaint_id" })
+  complaint?: Complaint;
+
+  // 공지사항
+  // @ManyToOne(() => Notice, (notice) => notice.comments, { nullable: true, onDelete: "CASCADE" })
+  // @JoinColumn({ name: "notice_id" })
+  // notice?: Notice;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt!: Date;
+}
