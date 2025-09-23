1b9c40f1eedc56dcafb376275c5120e6b38f09bf
siyeon kang <rkdtldus0115@naver.com>
2025-09-23 09:37:41 +0900

    fix: 임의 저장

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { notFoundHandler } from './handler/not-found.handler';
 import root from './root/root.router';
 import { globalErrorHandler } from './handler/global-error.handler';
 import auth from './auth/auth.router';
+import complaint from './complaint/complaint.router';
 
 const app: Application = express();
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use('/api', root);
 app.use('/api/auth', auth);
+app.use('/api/complaint', complaint);
 
 app.use(notFoundHandler);
 app.use(globalErrorHandler);
